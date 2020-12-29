3c968bee8ad735e88887838760bce6f35ed410d9
Dinesh Singh <dinesh1985singh@gmail.com>
2020-12-29 13:19:11 +0530

    further fixes

--- feature-4.php
+++ feature-4.php
@@ -2,5 +2,7 @@
 /**
  * Adding feature 4.
  *
+ * @package github-actions
  */
+
 echo 'additioal feature';
--- master.php
+++ master.php
@@ -2,6 +2,7 @@
 /**
  * Main entry point.
  *
+ * @package  github-actions
  */
 
 echo 'inside main';
